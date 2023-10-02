5330f28c153851dae3e298507fe2cd540e665347
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2023-10-02 13:27:32 +0200

    index > articles : création du lien vers page photographe

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -6,38 +6,57 @@ function photographerTemplate(data) {
     // création des cartes des photographes
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        //!ajout d'un lien
-        //! ajout d'un conteneur image
+        const photographerArticle = document.createElement( 'article' );
+
+        //ajout d'un lien (contenant le portrait et le nom)
+        const photographerPageLink = document.createElement( 'a' );
+        photographerPageLink.className = 'photographerPageLink'
+        photographerPageLink.setAttribute('href', './photographer_page.html');
+        photographerPageLink.setAttribute('alt','lien vers la page du photographe');
+        
+        
+        // portrait : ajout d'un conteneur image + img
+
         const portraitBox = document.createElement('div');
         portraitBox.className = 'portraitBox';
-        // portrait
+
         const photographerPortrait = document.createElement( 'img' );
         photographerPortrait.setAttribute("src", picture);
         photographerPortrait.className = 'photographerPortrait'
+       
         //  name (prénom + nom)
         const photographerName = document.createElement( 'h2' );
         photographerName.textContent = name;
         photographerName.className ='photographerName'
 
+        // ville, pays
         const photographerLocation = document.createElement( 'p' );
         photographerLocation.className = 'photographerLocation';
         photographerLocation.textContent = `${city}, ${country}`; 
-
+        
+        // Slogan
         const photographerTagline = document.createElement( 'p' );
         photographerTagline.className = 'photographerTagline';
         photographerTagline.innerText = `${tagline}`;
 
+        // tarif
         const photographerPrice = document.createElement( 'p' );
         photographerPrice.className ='photographerPrice';
         photographerPrice.innerText = `${price}€/jour`;
+
+        // création de la branche du DOM
+        photographerPageLink.appendChild(portraitBox);
+        photographerPageLink.appendChild(photographerName);
         portraitBox.appendChild(photographerPortrait);
-        article.appendChild(portraitBox);
-        article.appendChild(photographerName);
-        article.appendChild(photographerLocation);
-        article.appendChild(photographerTagline);
-        article.appendChild(photographerPrice);
-        return (article);
+        // photographerArticle.appendChild(portraitBox);
+        // photographerArticle.appendChild(photographerName);
+        photographerArticle.appendChild(photographerPageLink);
+        photographerArticle.appendChild(photographerLocation);
+        photographerArticle.appendChild(photographerTagline);
+        photographerArticle.appendChild(photographerPrice);
+        
+        return (photographerArticle);
     }
+
     return { name, picture, getUserCardDOM }
 }
