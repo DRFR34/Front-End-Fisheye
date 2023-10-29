a200e9002336b35c9211b6755a4d2e3123f770e8
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2023-10-29 17:11:06 +0100

    MAJ mediasTemplates WIP
    fonctionnel

--- scripts/templates/mediasTemplates.js
+++ scripts/templates/mediasTemplates.js
@@ -1,86 +1,42 @@
-//  function createPhotoCard(data) {
-//     // destructuration
-//     const { id, photographerId, title, image,likes} = data;
-//     const photoCard = document.createElement( 'article' );
-//     photoCard.className='photoCard';
-//     photoCard.innerHTML = ` 
-//         <div class ="photoBox">           
-//         <img class='media-${id}' tabindex="0" alt="${title}" src="./assets/fisheye_photos/sample_photos/${photographerId}/${image}" alt="${title}">      
-//         </div>
-//         <div class="mediaInfos">
-//             <h3>${title}</h3>
-//             <div class="likesContainer">
-//                 <p class="mediaLikes">${likes}</p>
-//                 <i class="mediaCardIcon">&hearts;</i>            
-//         </div>
-//     </div>           
-//     `;      
-//     return photoCard;
-// }
+
 
 class PhotoCard {
     constructor(data) {
-        const { id, photographerId, title, image, likes } = data;
+        const { id, photographerId, title, image, likes, date } = data;
         this.photoCard = document.createElement('article');
-        this.photoCard.className = 'photoCard';
+        this.photoCard.className = 'mediaCard photoCard';
         this.photoCard.innerHTML = ` 
-                <div class ="photoBox">           
-                <img class='media-${id}' tabindex="0" alt="${title}" src="./assets/fisheye_photos/sample_photos/${photographerId}/${image}" alt="${title}">      
+            <div class ="mediaBox photoBox">           
+                <img class="media image" tabindex="0" alt="${title}" src="./assets/fisheye_photos/sample_photos/${photographerId}/${image}" alt="${title}">      
                  </div>
                 <div class="mediaInfos">
                     <h3>${title}</h3>
+                    <span class=" mediaDate">${date}</span>
                     <div class="likesContainer">
-                        <p class="mediaLikes">${likes}</p>
-                         <i class="mediaCardIcon">&hearts;</i>            
+                        <p class="mediaLikes ">${likes}</p>
+                         <i class="cardLikesHeart fa-regular fa-heart"></i>            
                  </div>
              </div>           
-             `;   
+             `;
     }
 }
 
 
-
-//**  function createVideoCard(data) {
-//     // destructuration
-//     const {id, photographerId, title, video, likes} = data;
-//     const videoCard = document.createElement( 'article' );
-//     videoCard.className='videoCard';
-//     videoCard.innerHTML = ` 
-//         <div class ="videoBox">           
-//             <video src="./assets/fisheye_photos/sample_photos/${photographerId}/${video}" controls tabindex="0">
-//         </div>
-//         <div class="mediaInfos">
-//             <h3>${title}</h3>
-//             <div class="likesContainer">
-//                 <p class="mediaLikes">${likes}</p>
-//                 <i class="mediaCardIcon">&hearts;</i>            
-//         </div>
-//     </div>           
-//     `;      
-    
-//     return videoCard;
-// }
-
-//
-
-
-//
-
-
 class VideoCard {
     constructor(data) {
-        const { id, photographerId, title, video, likes } = data;
+        const { id, photographerId, title, video, likes, date } = data;
         this.videoCard = document.createElement('article');
-        this.videoCard.className = 'videoCard';
+        this.videoCard.className = 'mediaCard videoCard';
         this.videoCard.innerHTML = `
-            <div class="videoBox">
-                <video src="./assets/fisheye_photos/sample_photos/${photographerId}/${video}" controls tabindex="0"></video>
+            <div class="mediaBox videoBox" >
+                <video class="media video" src="./assets/fisheye_photos/sample_photos/${photographerId}/${video}" tabindex="0"></video>
             </div>
             <div class="mediaInfos">
                 <h3>${title}</h3>
                 <div class="likesContainer">
+                    <span class=" mediaDate">${date}</span>
                     <p class="mediaLikes">${likes}</p>
-                    <i class="mediaCardIcon">&hearts;</i>
+                    <i class="cardLikesHeart fa-regular fa-heart"></i>
                 </div>
             </div>
         `;
@@ -89,4 +45,3 @@ class VideoCard {
 
 
 
-//******************* calcuul des likes ********************** */
