6c5672d52589eedc57646b72ac05434a2fe58c80
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2023-10-31 15:34:56 +0100

    WIP ébauche modale de contact vue en mentorat

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,13 @@
+
+const contactButton = document.querySelector(".contactButton");
+
+contactButton.addEventListener("click", displayModal);
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
+    const modal = document.getElementById("contactModal");
 	modal.style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
+    const modal = document.getElementById("contactModal");
     modal.style.display = "none";
 }
