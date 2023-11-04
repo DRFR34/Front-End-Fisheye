4c4efbdee479e3268076189bc4644ee1ec4fe46f
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2023-11-04 18:28:33 +0100

    WIP MAJ likeProcessing.js

--- scripts/utils/likesProcessing.js
+++ scripts/utils/likesProcessing.js
@@ -8,44 +8,43 @@ function calcTotalOfLikes() {
 
     const tempTotalOfLikes = Array.from(displayedLikesList).reduce((total, displayedLikes) => total + parseInt(displayedLikes.textContent), 0);
 
-            console.log("tempTotalOfLikes : ", tempTotalOfLikes);                
+            // console.log("tempTotalOfLikes : ", tempTotalOfLikes);                
         
-    totalLikes.textContent = tempTotalOfLikes       
+    totalLikes.textContent = tempTotalOfLikes ;      
     
 }
 
 //+1 si carte non cliquée, sinon -1 ;
 // + ou -  la classe clickedHeart ( → affichage d'un coeur plein)
-function addOrRemoveLikes() {
+function voteWithLikes() {
     const cardLikesHeartList = document.querySelectorAll('.cardLikesHeart');
 
     cardLikesHeartList.forEach(cardLikesHeart => {
-        
-        let yetClicked = false;
-    
-        cardLikesHeart.addEventListener('click', () => {
-           
-            const mediaLikes = cardLikesHeart.parentElement.querySelector('.mediaLikes');
-            let nbOFlikes = parseInt(mediaLikes.textContent);
-    
-            if (!yetClicked) {
-                
-                mediaLikes.textContent = nbOFlikes + 1;
-                yetClicked = true;
-                cardLikesHeart.classList= "cardLikesHeart clickedHeart fa-solid fa-heart";('clickedHeart')                
-            } else {
-                
-                mediaLikes.textContent = nbOFlikes - 1;
-                yetClicked = false;
-                cardLikesHeart.classList ="cardLikesHeart fa-regular fa-heart";
-            }
-            calcTotalOfLikes();
-        });
+        cardLikesHeart.addEventListener('click',() => addOrRemoveLikes(cardLikesHeart) );
+        cardLikesHeart.addEventListener('keydown', (event) => (event.key === 'Enter') ? addOrRemoveLikes(cardLikesHeart) : null );
     });
-
 }
+
+function addOrRemoveLikes(cardLikesHeart) {
+    const mediaLikes = cardLikesHeart.parentElement.querySelector('.mediaLikes');
+    let nbOFlikes = parseInt(mediaLikes.textContent);
+    if ( cardLikesHeart.classList.contains("fa-regular")) {
+        mediaLikes.textContent = nbOFlikes + 1;
+        cardLikesHeart.classList.remove("fa-regular");       
+        cardLikesHeart.classList.add("fa-solid");     
+    } else {
+        mediaLikes.textContent = nbOFlikes - 1;
+        cardLikesHeart.classList.remove("fa-solid");       
+        cardLikesHeart.classList.add("fa-regular");  
+    }
+
+    calcTotalOfLikes();
+}
+  
+
  // tempo pour création du Dom par JS
 setTimeout(() => { 
     calcTotalOfLikes();
-    addOrRemoveLikes();
+    voteWithLikes() 
+    // addOrRemoveLikes();
     }, 200);
