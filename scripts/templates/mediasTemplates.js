62d0a19bc4e8c932ccfdb56325ee0bdfe98784cf
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2023-11-11 22:08:44 +0100

    Update mediasTemplates.js

--- scripts/templates/mediasTemplates.js
+++ scripts/templates/mediasTemplates.js
@@ -2,21 +2,22 @@
 
 class PhotoCard {
     constructor(data) {
-        const { id, photographerId, title, image, likes, date } = data;
+        const { photographerId, title, image, likes, date } = data;
         this.photoCard = document.createElement('article');
         this.photoCard.className = 'mediaCard photoCard';
         this.photoCard.innerHTML = ` 
             <div class ="mediaBox photoBox">           
-                <img class="media image" tabindex="0" alt="${title}" src="./assets/fisheye_photos/sample_photos/${photographerId}/${image}" alt="${title}">      
-                 </div>
-                <div class="mediaInfos">
-                    <h3>${title}</h3>
-                    <span class=" mediaDate">${date}</span>
-                    <div class="likesContainer">
-                        <p class="mediaLikes ">${likes}</p>
-                         <i class="cardLikesHeart fa-regular fa-heart"></i>            
-                 </div>
-             </div>           
+                <img class="media image" tabindex="0" alt="${title}" src="./assets/fisheye_photos/sample_photos/${photographerId}/${image}" alt="Photo intitulée${title}" >      
+            </div>
+            <div class="mediaInfos">
+                <h3>${title}</h3>
+                <span class=" mediaDate">${date}</span>
+                <div class="likesContainer">
+                    <span class="invisible">Nombre de mentions j'aime :</span>
+                    <p class="mediaLikes" aria-label="Nombre de mentions j'aime : ${likes}" tabindex="0" >${likes}</p>
+                    <i class="cardLikesHeart fa-regular fa-heart" tabindex="0" aria-label="Cliquez pour ajouter une mention j'aime"></i>            
+                </div>
+            </div>           
              `;
     }
 }
@@ -24,19 +25,20 @@ class PhotoCard {
 
 class VideoCard {
     constructor(data) {
-        const { id, photographerId, title, video, likes, date } = data;
+        const { photographerId, title, video, likes, date } = data;
         this.videoCard = document.createElement('article');
         this.videoCard.className = 'mediaCard videoCard';
         this.videoCard.innerHTML = `
             <div class="mediaBox videoBox">
-                <video class="media video" src="./assets/fisheye_photos/sample_photos/${photographerId}/${video}" tabindex="0">video intitulée ${title} </video>
+                <video class="media video" src="./assets/fisheye_photos/sample_photos/${photographerId}/${video}" aria-label="video intitulée ${title}" tabindex="0">video intitulée ${title} </video>
             </div>
             <div class="mediaInfos">
                 <h3>${title}</h3>
                 <div class="likesContainer">
                     <span class=" mediaDate">${date}</span>
-                    <p class="mediaLikes">${likes}</p>
-                    <i class="cardLikesHeart fa-regular fa-heart"></i>
+                    <span class="invisible">Nombre de mentions j'aime :</span>
+                    <p class="mediaLikes" aria-label="Nombre de mentions j'aime ${likes}">${likes}</p>
+                    <i class="cardLikesHeart fa-regular fa-heart" aria-label="Cliquez pour ajouter une mention j'aime"></i>
                 </div>
             </div>
         `;
