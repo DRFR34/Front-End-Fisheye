af4178dd34d6d03bc00978d317c0f84b14158699
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2023-11-12 13:48:30 +0100

    Update photographersTemplates.js
    WCAG : mise en cohérence des niveaux de titres

--- scripts/templates/photographersTemplates.js
+++ scripts/templates/photographersTemplates.js
@@ -12,9 +12,9 @@
             </div>
             <h2 class="photographerName" aria-label="Nom du photographe:${name}" tabindex="-1">${name}</h2>
         </a>
-        <h4 class="photographerLocation" aria-label="Réside à ${city}, pays: ${country}" tabindex="0">${city}, ${country}</h4>
-        <h4 class="photographerTagline" aria-label="Sa devise :${tagline}"  tabindex="0">${tagline}</h4>
-        <h4 class="photographerPrice" aria-label="Tarif : ${price}€ par jour"  tabindex="0">${price}€/jour</h4>            
+        <h3 class="photographerLocation" aria-label="Réside à ${city}, pays: ${country}" tabindex="0">${city}, ${country}</h3>
+        <h3 class="photographerTagline" aria-label="Sa devise :${tagline}"  tabindex="0">${tagline}</h3>
+        <h3 class="photographerPrice" aria-label="Tarif : ${price}€ par jour"  tabindex="0">${price}€/jour</h3>            
     `;      
     
     return photographerArticle;
@@ -30,8 +30,8 @@
     photographerBanner.innerHTML = `
         <div id="textsContentBox"  class="textsContentBox">
             <h1 id="pagePhotographerName" class="photographerName" aria-label="Nom du photographe : ${name}" tabindex="0" >${name}</h1>
-            <h4 class="photographerLocation" aria-label="Ville : ${city} , pays : ${country}" tabindex="0"> ${city}, ${country}</h4>
-            <h4 class="photographerTagline" aria-label="slogan du photographe: ${tagline}" tabindex="0" aria-label="slogan du photographe:${tagline}">${tagline}</h4>            
+            <h2 class="photographerLocation" aria-label="Ville : ${city} , pays : ${country}" tabindex="0"> ${city}, ${country}</h2>
+            <h2 class="photographerTagline" aria-label="slogan du photographe: ${tagline}" tabindex="0" aria-label="slogan du photographe:${tagline}">${tagline}</h2>            
         </div>       
         <button id="contactButton" class="contactButton" type="button" role="button" aria-labelledby = "span${id}">Contactez-moi</button>
         <span id="span${id}" class="invisible">Pour envoyer un message à ${name}, cliquer sur ce bouton </span>
